767de0d3734bc07a963f4f3e693b52e6383ebda9
notsogaurab <gauravupreti1230@gmail.com>
2024-04-02 16:52:58 +0545

    github modules added

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 module "s3_bucket" {
-    source = "./modules/service/s3"
+    source = "git@github.com:notsogaurab/terraform-frontend.git//modules/service/s3"
     gaurab-test-s3-bucket = "gaurab-test-s3-bucket"
 }
 
 module "cloudfront_distribution" {
-    source = "./modules/service/cloudfront"
+    source = "git@github.com:notsogaurab/terraform-frontend.git//modules/service/cloudfront"
     
 }
 
 module "cloudflare" {
-    source = "./modules/service/cloudflare"
+    source = "git@github.com:notsogaurab/terraform-frontend.git//modules/service/cloudflare"
     domain_name = "gaurabupreti.com"
     cloudfront_domain_name = module.cloudfront_distribution.cloudflare_dns
 
